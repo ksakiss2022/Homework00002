2c2968c9ae4d98d06ffa8f39f3cc24e4d39b4236
Swetlana <sweta_zaika@mail.ru>
2022-08-06 19:14:06 +0300

    Домашнаяя работа к уроку 2

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        //  Задание 1
+        byte box = 1;
+        short rag = 35;
+        int mirrow = 500;
+        long watch = 10_000l;
+        float sugar = 10.12f;
+        double ring = 20.99999999999;
+        boolean smallRing = ring < 21;
+        char music = 14;
+
+        System.out.println(smallRing);
+    }
+}
