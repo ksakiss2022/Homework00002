53f7d2303914a6908491f0d00f895a4aa9810da0
Swetlana <sweta_zaika@mail.ru>
2022-08-06 20:19:27 +0300

    задания 1,2,3

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         //  Задание 1
-        byte box = 1;
         short rag = 35;
+        byte box = 3;
         int mirrow = 500;
         long watch = 10_000l;
         float sugar = 10.12f;
@@ -10,6 +10,24 @@ public class Main {
         boolean smallRing = ring < 21;
         char music = 14;
 
-        System.out.println(smallRing);
+        // Задание 2
+        double man1 = 78.2;
+        double man2 = 82.7;
+        double totalWeight = man1 + man2;
+        double weightDifference = man2 - man1;
+
+                System.out.println("Ответ задача 2: вес двух боксеров - " + totalWeight + " кг." +
+                        ", разница в весе боксеров - " + weightDifference + " кг.");
+
+          // Задание 3
+        int bananas = 80 * 5;
+        int milk = (200 / 100) * 105;
+        int ice = 100 * 2;
+        int egg = 4 * 70;
+        double breakfast = bananas + milk + ice + egg;
+        double breakfastKg = breakfast / 1000;
+                System.out.println("Ответ задача 3: Вес завтрака в граммах = " + breakfast +
+                        ", вес завтрака в килограммах = " + breakfastKg);
+
     }
 }
